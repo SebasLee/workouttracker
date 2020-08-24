f2f1525e386e936ae015a23f651d7fef0f77fdf0
SebastianLee <heechanlee98@gmail.com>
2020-08-24 13:57:14 -0700

    troubles with server

---
+++ controllers/pushController.js
@@ -0,0 +1,14 @@
+/*
+const express = require('express'); 
+const app = express();
+
+const PUSH = require('../models/push.js');
+
+router.get('/', (req, res) => {
+    res.render('index', {
+      pushes: PUSH,
+    })
+  });
+
+module.exports = router;
+*/
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require('express'); 
 const app = express();
 
-app.listen(3000, ()=>{
+//const CHEST = require('./models/chestTri');
+
+app.set('view engine', 'ejs');
+
+const port = 4000;
+
+/*
+app.get ('/chestTri/', (req, res) => {
+  res.send
+})
+*/
+app.get('/', (req,res)=>{
+    res.render('index');
+  });
+
+//app.use('/pushes', pushController); 
+
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page Not Found</h1>');
+  });
+
+app.listen(4000, ()=>{
     console.log("I am listening");
 });

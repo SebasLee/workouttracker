acc799251c907279ad0efd70fc6affc29e2806ff
SebastianLee <heechanlee98@gmail.com>
2020-08-27 11:32:38 -0700

    deploying to heroku, edits later

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -2,15 +2,12 @@ const express = require('express');
 const db = require('../models');
 const router = express.Router();
 
-// WORKOUT MODEL
 const WORKOUT = require('../models/Workout');
 
-// New Workout 
 router.get('/new', (req, res) => {
   res.render('new');
 });
 
-// Create Workout
 router.post('/', (req, res) => {
   console.log(req.body);
   db.Workout.create(req.body, (err, newWorkout) => {
@@ -19,7 +16,6 @@ router.post('/', (req, res) => {
   });
 });
 
-// One Single Workout Route (Show)
 router.get('/:id', (req, res) => {
   db.Workout.findById(req.params.id, (err, foundWorkout) => {
     if (err) return console.log(err);
@@ -29,7 +25,6 @@ router.get('/:id', (req, res) => {
   });
 });
 
-// All Workoutss Route (Index)
 router.get('/', (req, res) => {
   db.Workout.find({}, (err, allWorkout) => {
     if (err) return console.log(err);
@@ -40,7 +35,6 @@ router.get('/', (req, res) => {
   });
 });
 
-// Edit One Workout (Edit)
 router.get('/:id/edit', (req, res) => {
   db.Workout.findById(req.params.id, (err, foundWorkout) => {
     if (err) return console.log(err);
@@ -50,7 +44,6 @@ router.get('/:id/edit', (req, res) => {
   })
 });
 
-// Update One Workout (Update)
 router.put('/:id', (req, res) => {
   db.Workout.findByIdAndUpdate(
     req.params.id,
@@ -64,7 +57,6 @@ router.put('/:id', (req, res) => {
   );
 });
 
-// Delete One Workout
 router.delete('/:id', (req, res) => {
   db.Workout.findByIdAndDelete(req.params.id, (err, deletedWorkout) => {
     if (err) return console.log(err);
--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/workout';
 
-// Connect to MongoDB
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -11,7 +10,6 @@ mongoose.connect(MONGODB_URI, {
   .then(() => console.log(`MongoDB connected successfully at ${MONGODB_URI}`)) // Success Handler
   .catch((err) => console.log(`MongoDB connection error: ${err}`)) // Error Handler
 
-// MAKE ALL MODELS AVAILABLE
 module.exports = {
   Workout: require('./Workout'),
 };

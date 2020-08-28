80870eff09a0845b032f326bc3748dafe4d77740
SebastianLee <heechanlee98@gmail.com>
2020-08-27 20:54:22 -0700

    checkboxes on the start

--- models/Workout.js
+++ models/Workout.js
@@ -7,7 +7,43 @@ const workoutSchema = new mongoose.Schema({
     },
     cardio: {
         type: String,
-    }
+    },
+    jumpingjacks: {
+        type: Boolean,
+    },
+    burpees: {
+        type: Boolean,
+    },
+    pushups: {
+        type: Boolean,
+    },
+    dips: {
+        type: Boolean,
+    },
+    benchpress: {
+        type: Boolean,
+    },
+    pullups: {
+        type: Boolean,
+    },
+    dumbbellrow: {
+        type: Boolean,
+    },
+    superman: {
+        type: Boolean,
+    },
+    crunches: {
+        type: Boolean,
+    },
+    bicycle: {
+        type: Boolean,
+    },
+    leglifts: {
+        type: Boolean,
+    },
+    planks: {
+        type: Boolean,
+    },
 }, {timestamps: true});
 
 const Workout = mongoose.model('Workout', workoutSchema);
